0328adb1647602de99654cbd4c555da65a0c091e
samSargsyan <samuelsargsyan2010@gmail.com>
2023-12-10 19:32:14 -0800

    fix redirect part

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header/Header';
 import Content from './components/content/Content';
 import Footer from './components/footer/Footer';
 import About from './components/about/About';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Contacts from './components/contacts/Contacts';
 import Product from './components/product/Product';
 import items from './components/items';
@@ -16,11 +16,11 @@ function App() {
       <Header />
         <Routes>
           <Route index path="/" element={<Content />} />
+          <Route path='*' element={<Navigate to='/' />} />
           <Route path="/about" element={<About />} />
           <Route path="/specialDeals" element={<SpecialDeals />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/product/:id" element={<Product items={items} />} />
-
         </Routes>
       <Footer />
     </div>
