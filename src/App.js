1d85f501d82aa93391397fbb8013a9ab700971b9
samSargsyan <samuelsargsyan2010@gmail.com>
2025-01-21 19:38:29 -0800

    fix route

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Contacts from './components/contacts/Contacts';
 import Product from './components/product/Product';
 import items from './components/items';
+import { BrowserRouter } from "react-router-dom";
 import SpecialDeals from './components/specialDeals/SpecialDeals';
 
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
       <Header />
-        <Routes>
-          <Route index path="/" element={<Content />} />
-          <Route path='*' element={<Navigate to='/' />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/specialDeals" element={<SpecialDeals />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/product/:id" element={<Product items={items} />} />
-        </Routes>
+      <Routes>
+        <Route exact path="/toy-store.github.io" element={<Content />} />
+        <Route exact path="/" element={<Content />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/specialDeals" element={<SpecialDeals />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/product/:id" element={<Product items={items} />} />
+      </Routes>
       <Footer />
+    </BrowserRouter>
     </div>
   );
 }
