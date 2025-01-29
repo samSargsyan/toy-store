c6067e5f0a2f67d1ac2b6c9b8cdcccd9a7326f56
samSargsyan <samuelsargsyan2010@gmail.com>
2025-01-28 18:18:29 -0800

    change repasitory name

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import './App.css';
-import Header from './components/header/Header';
-import Content from './components/content/Content';
-import Footer from './components/footer/Footer';
-import About from './components/about/About';
+import "./App.css";
+import Header from "./components/header/Header";
+import Content from "./components/content/Content";
+import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
 import { Routes, Route } from "react-router-dom";
-import Contacts from './components/contacts/Contacts';
-import Product from './components/product/Product';
-import items from './components/items';
+import Contacts from "./components/contacts/Contacts";
+import Product from "./components/product/Product";
+import items from "./components/items";
 import { BrowserRouter } from "react-router-dom";
-import SpecialDeals from './components/specialDeals/SpecialDeals';
-
+import SpecialDeals from "./components/specialDeals/SpecialDeals";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/toy-store.github.io" element={<Content />} />
-        <Route index path="/" element={<Content />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/specialDeals" element={<SpecialDeals />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/product/:id" element={<Product items={items} />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Content />} />
+          <Route path="/toy-store" element={<Content />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/specialDeals" element={<SpecialDeals />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/product/:id" element={<Product items={items} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
-
-
 
 export default App;
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,38 +1,33 @@
 import React from "react";
 import "./content.scss";
-import Carousel from 'react-material-ui-carousel'
-import { Paper } from '@mui/material'
-import drawer from '../../assets/images/closeUpDrwer.png';
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+import drawer from "../../assets/images/closeUpDrwer.png";
 import { Link } from "react-router-dom";
-import items from '../items';
+import items from "../items";
 
-
-function Content()
-{
-    return (
-        <div className="carousel-div">
-            <Carousel>
-            {
-                items.map( (item, i) => <Item key={item.id} item={item} alt={item.name} id={item.id}/> )
-            }
-            </Carousel>
-            <img src={drawer} className="drawer" alt={drawer}/>
+function Item({ item, key, alt, id }) {
+  return (
+    <Paper>
+      <Link to={`/product/${id}`} key={key}>
+        <div className="carousel-style">
+          <h2>{item.name}</h2>
+          <img src={item.src} height="300px" alt={alt} />
         </div>
-    )
+      </Link>
+    </Paper>
+  );
 }
 
-function Item({item,key,alt,id})
-{
-    return (
-        <Paper>
-            <Link to={`/product/${id}`} key={key}>
-                <div className='carousel-style'>
-                    <h2>{item.name}</h2>
-                    <img src={item.src} height="300px" alt={alt}/>
-                </div>
-            </Link>
-        </Paper>
-    )
+export default function Content() {
+  return (
+    <div className="carousel-div">
+      <Carousel>
+        {items.map((item, i) => (
+          <Item key={item.id} item={item} alt={item.name} id={item.id} />
+        ))}
+      </Carousel>
+      <img src={drawer} className="drawer" alt={drawer} />
+    </div>
+  );
 }
-
-export default Content;
